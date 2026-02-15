9c85694eaa5401d3d1cbbb523d320eda69d61001
Renan Ferreira <renan.ferrer@hotmail.com>
2026-02-15 11:35:13 -0300

    chore: ajusta configuração Cypress após refatoração estrutural

--- UI/cypress.config.js
+++ UI/cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
-    reportDir: 'cypress/reports/html',
+    reportDir: 'UI/cypress/reports/html',
     charts: true,
     reportPageTitle: 'EBAC Store E2E',
     embeddedScreenshots: true,
@@ -11,13 +11,18 @@ module.exports = defineConfig({
     overwrite: false,
     saveAllAttempts: false,
   },
+
   e2e: {
     baseUrl: 'http://lojaebac.ebaconline.art.br/',
+    specPattern: 'UI/cypress/e2e/**/*.cy.js',
+    fixturesFolder: 'UI/cypress/fixtures',
+
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
       return config;
     },
-    screenshotsFolder: 'cypress/reports/screenshots',
-    videosFolder: 'cypress/reports/videos',
+
+    screenshotsFolder: 'UI/cypress/reports/screenshots',
+    videosFolder: 'UI/cypress/reports/videos',
   },
 });

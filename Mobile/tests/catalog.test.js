70497981056d5739dbb48cc8b3e0ce9257c48416
Renan Ferreira <renan.ferrer@hotmail.com>
2026-02-18 13:50:47 -0300

    update

--- Mobile/tests/catalog.test.js
+++ Mobile/tests/catalog.test.js
@@ -4,16 +4,9 @@ const catalogPage = require('../pages/catalog.page');
 describe('Mobile - Catálogo de Produtos', () => {
 
     it('Deve validar navegação e preço do produto', async () => {
-
-        // Navegação com reset
         await catalogPage.goToBrowseWithReset();
-
-        // Esperar produtos carregarem
         await catalogPage.waitForProductsToLoad();
-
-        // Capturar preço
         const catalogPrice = await catalogPage.getCatalogPrice();
-
         console.log('Preço capturado:', catalogPrice);
 
         assert.ok(catalogPrice.includes('R$'), 'Preço não encontrado no catálogo');

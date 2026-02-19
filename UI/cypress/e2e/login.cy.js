d63468dd4b78a76ca93813fd0fb2d29d325d5a8a
Renan Ferreira <renan.ferrer@hotmail.com>
2026-02-18 21:16:43 -0300

    feat: ajustes finais login + geração dinâmica de usuário

--- UI/cypress/e2e/login.cy.js
+++ UI/cypress/e2e/login.cy.js
@@ -3,43 +3,59 @@
 import { loginPage } from '../support/pages/login.page';
 
 describe('US-0002 - Login na Plataforma', () => {
+
+  let user;
+
+  before(() => {
+    cy.createAccount();
+
+    cy.get('@createdUser').then((u) => {
+      user = u;
+    });
+
+    // Faz logout após criar conta
+    cy.contains('Logout').click();
+  });
+
   beforeEach(() => {
     loginPage.visit();
   });
 
   it('CT-007 - Deve fazer login com credenciais válidas', () => {
-    cy.fixture('data').then((dados) => {
-      loginPage.login(dados.email, dados.senha);
-      cy.get('body')
-        .should('contain', 'Welcome');
-      loginPage.logout();
-      cy.get('body')
-        .should('not.contain', 'Welcome')
-        .and('not.contain', 'Logout');
-    });
+    loginPage.login(user.email, user.password);
+
+    cy.get('body')
+      .should('contain', 'Logout');
+
+    loginPage.logout();
+
+    cy.get('body')
+      .should('not.contain', 'Logout');
   });
 
   it('CT-008 - Deve exibir erro ao informar senha inválida', () => {
-    cy.fixture('data').then((dados) => {
-      loginPage.login(dados.email, 'SenhaInvalida123');
-      loginPage.getErrorMessage()
-        .should('be.visible')
-        .and('contain', 'Erro');
-      cy.get('body')
-        .should('not.contain', 'Welcome');
-    });
+    loginPage.login(user.email, 'SenhaInvalida123');
+
+    loginPage.getErrorMessage()
+      .should('be.visible')
+      .and('contain', 'Erro');
+
+    cy.get('body')
+      .should('not.contain', 'Logout');
   });
 
   it('CT-010 - Não deve bloquear usuário após 3 tentativas inválidas (Regra não implementada)', () => {
-    cy.fixture('data').then((dados) => {
-      for (let i = 1; i <= 3; i++) {
-        loginPage.login(dados.email, 'SenhaInvalida123');
-        loginPage.getErrorMessage().should('be.visible');
-      }
-      loginPage.login(dados.email, 'SenhaInvalida123');
+
+    for (let i = 1; i <= 3; i++) {
+      loginPage.login(user.email, 'SenhaInvalida123');
       loginPage.getErrorMessage().should('be.visible');
-      cy.get('body')
-        .should('not.contain', 'bloqueado');
-    });
+    }
+
+    loginPage.login(user.email, 'SenhaInvalida123');
+    loginPage.getErrorMessage().should('be.visible');
+
+    cy.get('body')
+      .should('not.contain', 'bloqueado');
   });
+
 });
--- UI/cypress/support/commands.js
+++ UI/cypress/support/commands.js
@@ -2,11 +2,19 @@ import { cartPage } from './pages/cart.page';
 import { productPage } from './pages/product.page';
 import { loginPage } from './pages/login.page';
 
+const { uniqueEmail, uniquePassword } = require('./utils');
+
+// --------------------------------------------------
+// LIMPAR CARRINHO
+// --------------------------------------------------
 Cypress.Commands.add('clearCart', () => {
   cy.visit('/carrinho/');
   cartPage.removeFirstItemIfExists();
 });
 
+// --------------------------------------------------
+// ADICIONAR PRODUTO VARIÁVEL
+// --------------------------------------------------
 Cypress.Commands.add('addVariableProductToCart', ({
   slug,
   size,
@@ -37,10 +45,45 @@ Cypress.Commands.add('addVariableProductToCart', ({
   cy.get('.woocommerce-message', { timeout: 10000 })
     .should('be.visible')
     .and('contain', 'no seu carrinho');
+});
 
+// --------------------------------------------------
+// LOGIN
+// --------------------------------------------------
 Cypress.Commands.add('login', (email, password) => {
   loginPage.visit();
   loginPage.login(email, password);
 });
 
+// --------------------------------------------------
+// CRIAR CONTA DINÂMICA
+// --------------------------------------------------
+Cypress.Commands.add('createAccount', () => {
+
+  const email = uniqueEmail('qa.ebac', 'teste.com');
+  const password = uniquePassword(12);
+
+  cy.visit('/minha-conta/');
+
+  cy.get('#reg_email')
+    .should('be.visible')
+    .clear()
+    .type(email);
+
+  cy.get('#reg_password')
+    .should('be.visible')
+    .clear()
+    .type(password);
+
+  // BOTÃO REGISTER - seletor correto (input submit)
+  cy.get('input[name="register"]')
+    .should('be.visible')
+    .click();
+
+  // valida se entrou na conta
+  cy.get('body', { timeout: 10000 })
+    .should('contain', 'Logout');
+
+  // salva credenciais criadas
+  cy.wrap({ email, password }).as('createdUser');
 });
